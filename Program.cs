6d41b4d8037cd39b981db41a36b3b5f37289c184
Seyi-baba <137610818+Seyi-baba@users.noreply.github.com>
2025-03-16 19:45:05 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input First number");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Second number");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Symbol of Operation");
+string operation = Console.ReadLine() ?? "symbol";
+
+if(operation == "+")
+{
+    Console.WriteLine(num1 + num2);
+}
+else if(operation == "-")
+{
+    Console.WriteLine(num1 - num2);
+}
+else if(operation == "*")
+{
+    Console.WriteLine(num1 * num2);
+}
+else if(operation == "/")
+{
+    Console.WriteLine(num1 / num2);
+}
+else
+{
+    Console.WriteLine("Invalid Operation");
+}
